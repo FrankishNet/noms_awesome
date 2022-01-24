8da51a266dbc551f8efc2074dd7ff5cdd33ce9ca
frankishine <frankish@frankish.net>
2022-01-23 21:00:22 -0500

    reponse

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ let noms = [
     "Reebsa",
     "Underbough"
 ];
+
+
+function pogneRandom(tableau){
+    let index = Math.floor(Math.random() * tableau.length);
+    let choixRandom = tableau[index];
+    return choixRandom;
+}
+
+function getNumberOfNames(){
+    let nb = document.getElementById("nbNoms").value;
+    if( isNaN(Number(nb)) || nb === "" ) {
+        return 1;
+    }
+    return nb;
+}
+
+function generateRandomNoms(e){
+    e.preventDefault();
+    let nbNombs = getNumberOfNames();
+    let nomsResult = "";
+
+    for(let i = 0; i < nbNombs; i++){
+        let nom = pogneRandom(prenoms) + " " + pogneRandom(noms);
+        nomsResult = nomsResult + "<b>"+nom + "</b><br/>";
+    }
+    document.getElementById("listedenoms").innerHTML = nomsResult;
+}
+
+document.getElementById("mainForm").addEventListener("submit",generateRandomNoms);
